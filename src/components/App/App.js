4c55426fa5fe4f0947851be8a3523235a14731fe
CheNikos <chenikos@yandex.ru>
2023-04-20 23:05:45 +0300

    Доделал header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,21 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../Main/Main";
-import Movies from "../Movies/Movies.js"
+import Movies from "../Movies/Movies.js";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 function App() {
   return (
     <div className="app">
-      <Header />
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/movies" element={<Movies />} />
-      </Routes>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/movies" element={<Movies />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
-      <Footer />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,34 @@
-import './Header.css';
-import logo from '../../images/header-image/logo.svg'
+import "./Header.css";
+import logo from "../../images/header-image/logo.svg";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation();
 
   return (
-    <header className="header">
+    <header className={`header header_theme_${
+      location.pathname === '/' ? 'main' : 'grey'
+    }`}>
       <img className="header__logo" src={logo} alt="Logo" />
-      <div className='header__navigate'>
-        <p className='header__registration'>Регистрация</p>
-        <div className="header__signin"><p className='header__signin-text'>Войти</p></div>
-      </div>
+      {location.pathname === "/" && (
+        <div className="header__navigate">
+          <p className="header__registration">Регистрация</p>
+          <div className="header__signin">
+            <p className="header__signin-text">Войти</p>
+          </div>
+        </div>
+      )}
+      {location.pathname === "/movies" && (
+        <>
+        <div className="header__navigation-bar">
+          <button className="header__film">Фильмы</button>
+          <button className="header__saved-films">Сохранённые фильмы</button>
+        </div>
+        <div className="header__navigate">
+          <button className="header__account">Аккаунт</button>
+        </div>
+        </>
+      )}
     </header>
   );
 }
