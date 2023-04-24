a41d62301deb1e3898dce69dfe49e59bb992eade
CheNikos <chenikos@yandex.ru>
2023-04-24 23:42:18 +0300

    Доделал footer. Продолжаю верстать profile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,19 @@
+import { useLocation } from "react-router-dom";
 import "./Footer.css";
 
 export default function Footer() {
   let today = new Date();
   let year = today.getFullYear();
+  const location = useLocation();
 
   return (
-    <footer className="footer">
+      <footer className="footer">
+      {location.pathname === "/" ||
+          location.pathname === '/movies' ||
+          location.pathname === '/saved-movies' ? (
+        <>
       <p className="footer__text">
-        Учебный проект Яндекс.Практикум х BeatFilm.
+        Учебный проект Яндекс.Практикум x BeatFilm.
       </p>
       <div className="footer__line"></div>
       <div className="footer__stroke">
@@ -15,6 +21,8 @@ export default function Footer() {
         <p className="footer__link">Яндекс.Практикум</p>
         <p className="footer__link-git">Github</p>
       </div>
+      </>
+      ) : <></>}
     </footer>
   );
 }
