de99c6ea38a7d53bd435f17aa19d3fb07b36958f
CheNikos <chenikos@yandex.ru>
2023-05-07 23:51:55 +0300

    повторная итерация

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -4,7 +4,7 @@ export default function Portfolio() {
   return (
     <div className="portfolio">
         <h2 className="portfolio__title">Портфолио</h2>
-        <a href="https://chenikos.github.io/" target="blank" className="portfolio__website">
+        <a href="https://chenikos.github.io/how-to-learn/" target="blank" className="portfolio__website">
             <h3 className="portfolio__text">Статичный сайт</h3>
             <div className="portfolio__cursor">↗</div>
         </a>
@@ -14,7 +14,7 @@ export default function Portfolio() {
             <div className="portfolio__cursor">↗</div>
         </a>
         <div className="portfolio__line"></div>
-        <a href="https://chenikos.github.io/how-to-learn/" target="blank" className="portfolio__website">
+        <a href="https://chenikos.github.io/mesto/" target="blank" className="portfolio__website">
             <h3 className="portfolio__text">Одностраничное приложение</h3>
             <div className="portfolio__cursor">↗</div>
         </a>
