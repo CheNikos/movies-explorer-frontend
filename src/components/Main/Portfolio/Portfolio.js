c4bb4816326e29711a97a8cfa85c5735c904c446
CheNikos <chenikos@yandex.ru>
2023-05-09 19:52:10 +0300

    Исправил Portfolio

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -1,39 +1,70 @@
+import { Link } from "react-router-dom";
 import "./Portfolio.css";
 
 export default function Portfolio() {
   return (
     <div className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
-      <ul
-        href="https://chenikos.github.io/how-to-learn/"
-        target="blank"
-        className="portfolio__website"
-      >
+      <ul className="portfolio__website">
         <li className="portfolio__list">
-          <h3 className="portfolio__text">Статичный сайт</h3>
-          <div className="portfolio__cursor">↗</div>
+          <h3 className="portfolio__text">
+            <Link
+              to={"https://chenikos.github.io/how-to-learn/"}
+              target="blank"
+              className="portfolio__link"
+            >
+              Статичный сайт
+            </Link>
+          </h3>
+          <div className="portfolio__cursor">
+            <Link
+              to={"https://chenikos.github.io/how-to-learn/"}
+              target="blank"
+              className="portfolio__link"
+            >
+              ↗
+            </Link>
+          </div>
         </li>
-      </ul>
-      <div className="portfolio__line"></div>
-      <ul
-        href="https://chenikos.github.io/russian-travel/"
-        target="blank"
-        className="portfolio__website"
-      >
         <li className="portfolio__list">
-          <h3 className="portfolio__text">Адаптивный сайт</h3>
-          <div className="portfolio__cursor">↗</div>
+          <h3 className="portfolio__text">
+            <Link
+              to={"https://chenikos.github.io/russian-travel/"}
+              target="blank"
+              className="portfolio__link"
+            >
+              Адаптивный сайт
+            </Link>
+          </h3>
+          <div className="portfolio__cursor">
+            <Link
+              to={"https://chenikos.github.io/russian-travel/"}
+              target="blank"
+              className="portfolio__link"
+            >
+              ↗
+            </Link>
+          </div>
         </li>
-      </ul>
-      <div className="portfolio__line"></div>
-      <ul
-        href="https://chenikos.github.io/mesto/"
-        target="blank"
-        className="portfolio__website"
-      >
         <li className="portfolio__list">
-          <h3 className="portfolio__text">Одностраничное приложение</h3>
-          <div className="portfolio__cursor">↗</div>
+          <h3 className="portfolio__text">
+            <Link
+              to={"https://chenikos.github.io/mesto/"}
+              target="blank"
+              className="portfolio__link"
+            >
+              Одностраничное приложение
+            </Link>
+          </h3>
+          <div className="portfolio__cursor">
+            <Link
+              to={"https://chenikos.github.io/mesto/"}
+              target="blank"
+              className="portfolio__link"
+            >
+              ↗
+            </Link>
+          </div>
         </li>
       </ul>
     </div>
