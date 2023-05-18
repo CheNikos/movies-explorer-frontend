1710a87f69509bc92f4eb24fe50f36b7067199de
CheNikos <chenikos@yandex.ru>
2023-05-18 21:50:36 +0300

    Начал level-3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import Main from "../Main/Main";
 import Movies from "../Movies/Movies.js";
 import SavedMovies from "../Saved-movies/Saved-movies.js";
@@ -9,8 +9,22 @@ import Login from "../Login/Login";
 import PageNotFound from "../PageNotFound/PageNotFound";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import * as MainApi from "../utils/MainApi";
 
 function App() {
+  const navigate = useNavigate();
+
+  function handleRegister({ email, password, name }) {
+    return MainApi
+      .register(email, password, name)
+      .then(() => {
+        navigate("/signin");
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
   return (
     <div className="app">
       <BrowserRouter>
@@ -20,7 +34,7 @@ function App() {
           <Route path="/movies" element={<Movies />} />
           <Route path="/saved-movies" element={<SavedMovies />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/signup" element={<Register />} />
+          <Route path="/signup" handleRegister={handleRegister} element={<Register />} />
           <Route path="/signin" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.diploma.parkharidi.nomoredomains.monster/';
+// const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
