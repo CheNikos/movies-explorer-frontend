81a58631b2e5397d88938c79d566c6ef1dd5d920
CheNikos <chenikos@yandex.ru>
2023-06-22 12:12:27 +0300

    Вторая итерация

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import {Route, Routes, useLocation, useNavigate} from "react-router-dom";
-import {useState, useEffect} from "react";
-import {CurrentUserContext} from "../../contexts/CurrentUserContext.js";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext.js";
 import Main from "../Main/Main";
 import Movies from "../Movies/Movies.js";
 import SavedMovies from "../Saved-movies/Saved-movies.js";
@@ -18,232 +18,250 @@ import Preloader from "../Preloader/Preloader";
 import Modal from "../Modal/Modal";
 
 function App() {
-    const [currentUser, setCurrentUser] = useState({});
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [savedMovies, setSavedMovies] = useState([]);
-    const [cards, setCards] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [init, setInit] = useState(true);
-    const [error, setError] = useState();
-    const navigate = useNavigate();
-    const location = useLocation();
+  const [currentUser, setCurrentUser] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [savedMovies, setSavedMovies] = useState([]);
+  const [cards, setCards] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [init, setInit] = useState(true);
+  const [error, setError] = useState();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    useEffect(() => {
-        const token = localStorage.getItem('token')
-        if ((token && loggedIn) && !init) {
-            mainApi.getUserInfo(token)
-                .then((data) => {
-                    getMovies(token)
-                    getCards(token)
-                    setCurrentUser(data.data);
-                }).catch((err) => {
-                setError(err?.response?.data?.message)
-            })
-        }
-    }, [loggedIn])
-
-    useEffect(() => {
-        const token = localStorage.getItem('jwt')
-        if (token) {
-            setLoading(true)
-            mainApi.checkToken().then(() => {
-                mainApi.getUserInfo()
-                    .then((data) => {
-                        getMovies(token)
-                        getCards(token)
-                        setCurrentUser(data.data);
-                        setInit(false)
-                        navigate(location.pathname)
-                    })
-            }).finally(() => {
-                setLoading(false)
-                setInit(false)
-            });
-        }
-        if (!token) {
-            setInit(false)
-        }
-        // eslint-disable-next-line
-    }, []);
-
-
-    function handleRegister({name, email, password}) {
-        setLoading(true)
-        return mainApi
-            .register(name, email, password)
-            .then((data) => {
-                navigate("/signin");
-            })
-            .catch((err) => {
-                setError(err?.response?.data?.message)
-            }).finally(() => {
-                setLoading(false)
-            });
-    }
-
-    function getCards() {
-        const token = localStorage.getItem('jwt')
-        mainApi.getCards(token)
-            .then((data) => {
-                localStorage.setItem('savedMovies', JSON.stringify(data.data))
-                setSavedMovies(data.data);
-            })
-            .catch((err) => {
-                setError(err?.response?.data?.message)
-            }).finally(() => {
-            setLoading(false)
-        });
-    }
-
-    function getMovies(jwt, to = location.pathname) {
-        moviesApi.getMovies()
-            .then((allMovies) => {
-                setLoggedIn(true);
-                localStorage.setItem('allCards', JSON.stringify(allMovies))
-                setCards(allMovies)
-
-                navigate(to);
-            }).catch((err) => {
-            setError(err?.response?.data?.message)
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token && loggedIn && !init) {
+      mainApi
+        .getUserInfo(token)
+        .then((data) => {
+          getMovies(token);
+          getCards(token);
+          setCurrentUser(data.data);
         })
-    }
-
-    function handleLogin({email, password}) {
-        setLoading(true)
-        return mainApi
-            .login(email, password)
-            .then((datas) => {
-                if (datas.status === 200) {
-                    console.log(datas.data.jwt)
-                    localStorage.setItem("jwt", datas.data.jwt);
-                    getMovies(datas.data.jwt, '/movies')
-                    getCards(datas.data.jwt)
-                    setLoggedIn(true)
-                }
-            })
-            .catch((err) => {
-                setError(err?.response?.data?.message)
-            }).finally(() => {
-                setLoading(false)
-            });
-    }
-
-    function handleSingOut() {
-        localStorage.clear()
-        navigate('/signin')
-    }
-
-    function handleUpdateProfile({name, email}) {
-        setLoading(true)
-        mainApi
-            .updateUserInfo(name, email)
-            .then((newUserData) => {
-                setCurrentUser(newUserData.data);
-            })
-            .catch((err) => {
-                setError(err?.response?.data?.message)
-            }).finally(() => {
-            setLoading(false)
+        .catch((err) => {
+          setError(err?.response?.data?.message);
         });
     }
+    // eslint-disable-next-line
+  }, [loggedIn]);
 
-    function handleSaveMovie(card) {
-        const token = localStorage.getItem('jwt')
-        setLoading(true)
-        mainApi
-            .saveMovie(card, token)
-            .then((data) => {
-                debugger
-                localStorage.setItem('savedMovies', JSON.stringify([data.data, ...savedMovies]))
-                setSavedMovies([data.data, ...savedMovies]);
-            })
-            .catch((err) => {
-                setError(err?.response?.data?.message)
-            }).finally(() => {
-            setLoading(false)
+  useEffect(() => {
+    const token = localStorage.getItem("jwt");
+    if (token) {
+      setLoading(true);
+      mainApi
+        .checkToken()
+        .then(() => {
+          mainApi.getUserInfo().then((data) => {
+            getMovies(token);
+            getCards(token);
+            setCurrentUser(data.data);
+            setInit(false);
+            navigate(location.pathname);
+          });
+        })
+        .finally(() => {
+          setLoading(false);
+          setInit(false);
         });
     }
-
-    function handleDeleteMovie(card) {
-        setLoading(true)
-        mainApi
-            .deleteMovie(card._id)
-            .then((res) => {
-                debugger
-                const oldMovies = JSON.parse(localStorage.getItem('savedMovies'))
-                if(oldMovies) {
-                    const savedMovies = oldMovies?.filter((item) => item._id !== card._id)
-                    localStorage.setItem('savedMovies', JSON.stringify(savedMovies))
-                    setSavedMovies(savedMovies);
-                }
-
-            })
-            .catch((err) => {
-                setError(err?.response?.data?.message)
-            }).finally(() => {
-            setLoading(false)
-        });
+    if (!token) {
+      setInit(false);
     }
+    // eslint-disable-next-line
+  }, []);
 
+  function handleRegister({ name, email, password }) {
+    setLoading(true);
+    return mainApi
+      .register(name, email, password)
+      .then((data) => {
+        navigate("/signin");
+      })
+      .catch((err) => {
+        setError(err?.response?.data?.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
 
-    return (
-        <>
-            <div className="app">
-                <CurrentUserContext.Provider value={currentUser}>
-                    <Header loggedIn={loggedIn}/>
-                    <Routes>
-                        <Route path="/" element={<Main/>}/>
-                        <Route
-                            path="/movies"
-                            element={
-                                <ProtectedRoute
-                                    cards={cards}
-                                    loggedIn={loggedIn}
-                                    component={Movies}
-                                    onSaveMovie={handleSaveMovie}
-                                    onDeleteMovie={handleDeleteMovie}
-                                    savedMovies={savedMovies}
-                                />
-                            }
-                        />
-                        <Route
-                            path="/saved-movies"
-                            element={
-                                <ProtectedRoute
-                                    loggedIn={loggedIn}
-                                    component={SavedMovies}
-                                    onSaveMovie={handleSaveMovie}
-                                    savedMovies={savedMovies}
-                                    onDeleteMovie={handleDeleteMovie}
-                                />
-                            }
-                        />
-                        <Route
-                            path="/profile"
-                            element={
-                                <ProtectedRoute
-                                    loggedIn={loggedIn}
-                                    handleSingOut={handleSingOut}
-                                    component={Profile}
-                                    onSubmit={handleUpdateProfile}
-                                />
-                            }
-                        />
-                        <Route
-                            path="/signup"
-                            element={<Register handleRegister={handleRegister}/>}
-                        />
-                        <Route path="/signin" element={<Login setError={setError} handleLogin={handleLogin}/>}/>
-                        <Route path="*" element={<PageNotFound/>}/>
-                    </Routes>
-                    <Footer/>
-                </CurrentUserContext.Provider>
-                {(loading || init) && <Preloader/>}
+  function getCards() {
+    const token = localStorage.getItem("jwt");
+    mainApi
+      .getCards(token)
+      .then((data) => {
+        localStorage.setItem("savedMovies", JSON.stringify(data.data));
+        setSavedMovies(data.data);
+      })
+      .catch((err) => {
+        setError(err?.response?.data?.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
 
-            </div>
-            {error && <Modal onClose={() => setError(null)} error={error}/>}
-        </>
-    );
+  function getMovies(jwt, to = location.pathname) {
+    moviesApi
+      .getMovies()
+      .then((allMovies) => {
+        setLoggedIn(true);
+        localStorage.setItem("allCards", JSON.stringify(allMovies));
+        setCards(allMovies);
+
+        navigate(to);
+      })
+      .catch((err) => {
+        setError(err?.response?.data?.message);
+      });
+  }
+
+  function handleLogin({ email, password }) {
+    setLoading(true);
+    return mainApi
+      .login(email, password)
+      .then((datas) => {
+        if (datas.status === 200) {
+          console.log(datas.data.jwt);
+          localStorage.setItem("jwt", datas.data.jwt);
+          getMovies(datas.data.jwt, "/movies");
+          getCards(datas.data.jwt);
+          setLoggedIn(true);
+        }
+      })
+      .catch((err) => {
+        setError(err?.response?.data?.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  function handleSingOut() {
+    localStorage.clear();
+    navigate("/signin");
+  }
+
+  function handleUpdateProfile({ name, email }) {
+    setLoading(true);
+    mainApi
+      .updateUserInfo(name, email)
+      .then((newUserData) => {
+        setCurrentUser(newUserData.data);
+      })
+      .catch((err) => {
+        setError(err?.response?.data?.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  function handleSaveMovie(card) {
+    const token = localStorage.getItem("jwt");
+    setLoading(true);
+    mainApi
+      .saveMovie(card, token)
+      .then((data) => {
+        debugger;
+        localStorage.setItem(
+          "savedMovies",
+          JSON.stringify([data.data, ...savedMovies])
+        );
+        setSavedMovies([data.data, ...savedMovies]);
+      })
+      .catch((err) => {
+        setError(err?.response?.data?.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  function handleDeleteMovie(card) {
+    setLoading(true);
+    mainApi
+      .deleteMovie(card._id)
+      .then((res) => {
+        debugger;
+        const oldMovies = JSON.parse(localStorage.getItem("savedMovies"));
+        if (oldMovies) {
+          const savedMovies = oldMovies?.filter(
+            (item) => item._id !== card._id
+          );
+          localStorage.setItem("savedMovies", JSON.stringify(savedMovies));
+          setSavedMovies(savedMovies);
+        }
+      })
+      .catch((err) => {
+        setError(err?.response?.data?.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  return (
+    <>
+      <div className="app">
+        <CurrentUserContext.Provider value={currentUser}>
+          <Header loggedIn={loggedIn} />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route
+              path="/movies"
+              element={
+                <ProtectedRoute
+                  cards={cards}
+                  loggedIn={loggedIn}
+                  component={Movies}
+                  onSaveMovie={handleSaveMovie}
+                  onDeleteMovie={handleDeleteMovie}
+                  savedMovies={savedMovies}
+                />
+              }
+            />
+            <Route
+              path="/saved-movies"
+              element={
+                <ProtectedRoute
+                  loggedIn={loggedIn}
+                  component={SavedMovies}
+                  onSaveMovie={handleSaveMovie}
+                  savedMovies={savedMovies}
+                  onDeleteMovie={handleDeleteMovie}
+                />
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute
+                  loggedIn={loggedIn}
+                  handleSingOut={handleSingOut}
+                  component={Profile}
+                  onSubmit={handleUpdateProfile}
+                />
+              }
+            />
+            <Route
+              path="/signup"
+              element={<Register handleRegister={handleRegister} />}
+            />
+            <Route
+              path="/signin"
+              element={<Login setError={setError} handleLogin={handleLogin} />}
+            />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+          <Footer />
+        </CurrentUserContext.Provider>
+        {(loading || init) && <Preloader />}
+      </div>
+      {error && <Modal onClose={() => setError(null)} error={error} />}
+    </>
+  );
 }
 
 export default App;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,115 +1,117 @@
 import "./Register.css";
 import logo from "../../images/header-image/logo.svg";
-import {Link} from "react-router-dom";
-import {useState} from "react";
-import {validateEmail} from "../Login/Login";
-import {isEmailValid} from "../../utils/constants";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { isEmailValid } from "../../utils/constants";
 
-export default function Register({handleRegister}) {
-    const [userData, setUserData] = useState({
-        email: "pochta@yandex.ru",
-        password: "",
-        name: 'Николай'
+export default function Register({ handleRegister }) {
+  const [userData, setUserData] = useState({
+    email: "pochta@yandex.ru",
+    password: "",
+    name: "Николай",
+  });
+  const [isError, setIsError] = useState(false);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setIsError(false);
+
+    setUserData({
+      ...userData,
+      [name]: value,
     });
-    const [isError, setIsError] = useState(false);
+  }
 
-    function handleChange(e) {
-        const {name, value} = e.target;
-        setIsError(false)
-
-        setUserData({
-            ...userData,
-            [name]: value,
-        });
+  function handleSubmit(e) {
+    if (!(userData.password.length >= 3)) {
+      setIsError(true);
+      return;
     }
 
-    function handleSubmit(e) {
-        if (!(userData.password.length >= 3)) {
-            setIsError(true)
-            return
-        }
-
-        if (!isEmailValid(userData.email)) {
-            setIsError(true)
-            return
-        }
-        if (!userData.name.trim()) {
-            setIsError(true)
-            return
-        }
-
-        if (userData.password && userData.email && userData.name) {
-            handleRegister(userData)
-        }
+    if (!isEmailValid(userData.email)) {
+      setIsError(true);
+      return;
+    }
+    if (!userData.name.trim()) {
+      setIsError(true);
+      return;
     }
 
-    return (
-        <section className="signup">
-            <div className="signup__header">
-                <img className="signup__logo" src={logo} alt="Logo"/>
-                <h1 className="signup__title">Добро пожаловать!</h1>
-            </div>
-            <div className="signup__form">
-                <div className="signup__input-content">
-                    <label className="signup__label" htmlFor="name">
-                        Имя
-                    </label>
-                    <input
-                        className="signup__input"
-                        type="text"
-                        minLength="2"
-                        maxLength="30"
-                        autoComplete="on"
-                        name="name"
-                        value={userData.name}
-                        required
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="signup__input-content">
-                    <label className="signup__label" htmlFor="email">
-                        E-mail
-                    </label>
-                    <input
-                        className="signup__input"
-                        type="email"
-                        name="email"
-                        value={userData.email}
-                        onBlur={() => {
-                            if (!isEmailValid(userData.email)) {
-                                setIsError(true)
-                            }
-                        }}
-                        autoComplete="off"
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className="signup__input-content">
-                    <label className="signup__label" htmlFor="password">
-                        Пароль
-                    </label>
-                    <input
-                        className="signup__input"
-                        type="password"
-                        name="password"
-                        value={userData.password}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                {isError && <span className={'errorField'}>Что-то пошло не так...</span>
-                }
-                <button className="signup__edit" onClick={handleSubmit}>
-                    Зарегистрироваться
-                </button>
-            </div>
-            <p className="signup__footer">
-                Уже зарегистрированы?{" "}
-                <Link to={"/signin"}>
-                    <button className="signup__signin">Войти</button>
-                </Link>
-            </p>
-        </section>
-    );
+    if (userData.password && userData.email && userData.name) {
+      handleRegister(userData);
+    }
+  }
+
+  return (
+    <section className="signup">
+      <div className="signup__header">
+        <Link to={"/"}>
+          <img className="signup__logo" src={logo} alt="Logo" />
+        </Link>
+        <h1 className="signup__title">Добро пожаловать!</h1>
+      </div>
+      <div className="signup__form">
+        <div className="signup__input-content">
+          <label className="signup__label" htmlFor="name">
+            Имя
+          </label>
+          <input
+            className="signup__input"
+            type="text"
+            minLength="2"
+            maxLength="30"
+            autoComplete="on"
+            name="name"
+            value={userData.name}
+            required
+            onChange={handleChange}
+          />
+        </div>
+        <div className="signup__input-content">
+          <label className="signup__label" htmlFor="email">
+            E-mail
+          </label>
+          <input
+            className="signup__input"
+            type="email"
+            name="email"
+            value={userData.email}
+            onBlur={() => {
+              if (!isEmailValid(userData.email)) {
+                setIsError(true);
+              }
+            }}
+            autoComplete="off"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="signup__input-content">
+          <label className="signup__label" htmlFor="password">
+            Пароль
+          </label>
+          <input
+            className="signup__input"
+            type="password"
+            name="password"
+            value={userData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        {isError && (
+          <span className={"errorField"}>Что-то пошло не так...</span>
+        )}
+        <button className="signup__edit" onClick={handleSubmit}>
+          Зарегистрироваться
+        </button>
+      </div>
+      <p className="signup__footer">
+        Уже зарегистрированы?{" "}
+        <Link to={"/signin"}>
+          <button className="signup__signin">Войти</button>
+        </Link>
+      </p>
+    </section>
+  );
 }
